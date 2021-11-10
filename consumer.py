342fa530ce536e7c1fdbfb6011af29f2bbb9bf7f
Mahesh Chanduka <mahesh.chanduka@vanderbilt.edu>
2021-11-09 20:24:07 -0500

    milestone3

--- consumer.py
+++ consumer.py
@@ -14,39 +14,67 @@
 import os   # need this for popen
 import time # for sleep
 from kafka import KafkaConsumer  # consumer of events
+import uuid
+import couchdb
+import json
+import pycouchdb
+import ast
 
 # We can make this more sophisticated/elegant but for now it is just
 # hardcoded to the setup I have on my local VMs
 
+couchDBUser = "admin"
+couchDBPwd = "password"
+couchDBDB = "stock_data"
+
+couch = pycouchdb.Server( f"http://{couchDBUser}:{couchDBPwd}@3.139.135.215:5984")
+db = couch.create( couchDBDB )
+
 # acquire the consumer
 # (you will need to change this to your bootstrap server's IP addr)
-consumer = KafkaConsumer (bootstrap_servers="192.168.15.6:9092")
+consumer = KafkaConsumer (bootstrap_servers="3.139.135.215:9092,172.31.23.28:9092")
 
 # subscribe to topic
 consumer.subscribe (topics=["utilizations"])
 
 # we keep reading and printing
-for msg in consumer:
-    # what we get is a record. From this record, we are interested in printing
-    # the contents of the value field. We are sure that we get only the
-    # utilizations topic because that is the only topic we subscribed to.
-    # Otherwise we will need to demultiplex the incoming data according to the
-    # topic coming in.
-    #
-    # convert the value field into string (ASCII)
-    #
-    # Note that I am not showing code to obtain the incoming data as JSON
-    # nor am I showing any code to connect to a backend database sink to
-    # dump the incoming data. You will have to do that for the assignment.
-    print (str(msg.value, 'ascii'))
+try:    
+    for msg in consumer:
+        # what we get is a record. From this record, we are interested in printing
+        # the contents of the value field. We are sure that we get only the
+        # utilizations topic because that is the only topic we subscribed to.
+        # Otherwise we will need to demultiplex the incoming data according to the
+        # topic coming in.
+        #
+        # convert the value field into string (ASCII)
+        #
+        # Note that I am not showing code to obtain the incoming data as JSON
+        # nor am I showing any code to connect to a backend database sink to
+        # dump the incoming data. You will have to do that for the assignment.
+
+        #id = uuid.uuid1(
+        #print(msg)
+        print( f"--->>> Adding to DB {id}" )
+        
+        data=json.loads(str(msg.value,'ascii'))
+       
+        rec=ast.literal_eval(data)
+       # print(type(rec))
+        doc = db.save(rec)
+     
+        #print(doc) 
+
+except KeyboardInterrupt:
+    print(f"\n\nAdded to DB\n\n")
+    for id in db:
+        print( f"id={id}: {db[id]}" )
+        ### db.delete(db[id])
+
+#
+# Delete our database, since we do not care about data for the long term
+#
+couch.delete( couchDBDB )
 
 # we are done. As such, we are not going to get here as the above loop
 # is a forever loop.
 consumer.close ()
-    
-
-
-
-
-
-
--- producer.py
+++ producer.py
@@ -1,5 +1,3 @@
-#
-#
 # Author: Aniruddha Gokhale
 # CS4287-5287: Principles of Cloud Computing, Vanderbilt University
 #
@@ -17,44 +15,43 @@
 import os   # need this for popen
 import time # for sleep
 from kafka import KafkaProducer  # producer of events
-
-# We can make this more sophisticated/elegant but for now it is just
-# hardcoded to the setup I have on my local VMs
+import json
+import yfinance as yf
+ #hardcoded to the setup I have on my local VMs
 
 # acquire the producer
 # (you will need to change this to your bootstrap server's IP addr)
-producer = KafkaProducer (bootstrap_servers="192.168.15.6:9092", 
+producer = KafkaProducer (bootstrap_servers="3.139.135.215:9092", 
                                           acks=1)  # wait for leader to write to log
 
-# say we send the contents 100 times after a sleep of 1 sec in between
-for i in range (100):
-    
-    # get the output of the top command
-    process = os.popen ("top -n 1 -b")
+stock = ['AAPL','GOOG','MSFT','IBM','ORCL','BAC','C']
 
-    # read the contents that we wish to send as topic content
-    contents = process.read ()
+for i in range (7):
+    data = yf.Ticker(stock[i]).history(period='5y')
 
-    # send the contents under topic utilizations. Note that it expects
-    # the contents in bytes so we convert it to bytes.
+    #print(data.to_json())
+    #print(type(data))
+    j = json.dumps(data.to_json())
+    #print(type(j))
+
+
+           
+ # the contents in bytes so we convert it to bytes.
     #
     # Note that here I am not serializing the contents into JSON or anything
     # as such but just taking the output as received and sending it as bytes
     # You will need to modify it to send a JSON structure, say something
     # like <timestamp, contents of top>
     #
-    producer.send ("utilizations", value=bytes (contents, 'ascii'))
+
+    producer.send ("utilizations", value=bytes(j,'ascii'))
+
     producer.flush ()   # try to empty the sending buffer
 
     # sleep a second
+
     time.sleep (1)
 
 # we are done
 producer.close ()
     
-
-
-
-
-
-
